6a3009bfdbbc2ec289d37fdd9e6430de550ea0e7
Cyril <cyrildeveloppeur@gmail.com>
2021-09-03 16:31:02 +0200

    Fix Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,30 +20,30 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="var(--color-secondary-default)" />
-          <link rel="icon" href="./images/favicons/favicon.ico" />
+          <link rel="icon" href="/images/favicons/favicon.ico" />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="./images/favicons/favicon-16x16.png"
+            href="/images/favicons/favicon-16x16.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="./images/favicons/favicon-32x32.png"
+            href="/images/favicons/favicon-32x32.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="./images/favicons/apple-touch-icon.png"
+            href="/images/favicons/apple-touch-icon.png"
           />
           <link
             rel="mask-icon"
-            href="../public/images/favicons/safari-pinned-tab.svg"
+            href="/images/favicons/safari-pinned-tab.svg"
             color="#5bbad5"
           />
-          <link rel="manifest" href="./images/favicons/site.webmanifest" />
+          <link rel="manifest" href="/images/favicons/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#0f0e16" />
         </Head>
