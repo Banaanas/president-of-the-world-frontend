ce446fa39f142aa66190302c306c28b0d6fbe871
Cyril <52492538+Banaanas@users.noreply.github.com>
2022-04-06 17:45:18 +0200

    Update seo-data.ts

--- SEO/seo-data.ts
+++ SEO/seo-data.ts
@@ -23,16 +23,16 @@ const SEO: SEOProps = {
       "Log In to President of the World and submit your own Candidate.",
   },
   signUp: {
-    title: " President of the World  | Sign Up",
+    title: "President of the World | Sign Up",
     description:
       "Sign Up to President of the World and submit your own Candidate.",
   },
   myCandidate: {
-    title: " President of the World  | My Candidate",
+    title: "President of the World | My Candidate",
     description: "Submit your Candidate to be elected President of the World.",
   },
   updateCandidate: {
-    title: " President of the World  | Update Candidate",
+    title: "President of the World | Update Candidate",
     description: "Update your Candidate to be elected President of the World.",
   },
 };
