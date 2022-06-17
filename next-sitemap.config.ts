0e78809d64a79ed1824e7a4282da59c87c351d53
Cyril <cyrildeveloppeur@gmail.com>
2022-06-17 21:40:12 +0200

    fix: next-sitemap config file

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 module.exports = {
   siteUrl: "https://president-of-the-world.cyrilo.app/",
   generateRobotsTxt: true,
