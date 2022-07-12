41ff40844bfefa7b2625c814d0d980a32afe392d
Cyril <cyrildeveloppeur@gmail.com>
2022-07-12 21:50:30 +0200

    refactor: 404 and 500 Pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,15 +3,16 @@ import LinkButton from "../Components/LinkButton";
 import StyledErrorPagesContainer from "../Components/StyledComponents/StyledErrorPagesContainer";
 import StyledH1 from "../Components/StyledComponents/StyledH1";
 import StyledPageMain from "../Components/StyledComponents/StyledPageMain";
+import navLinks from "../data/navLinks";
 
-const Custom404 = () => (
+const Custom404Page = () => (
   <StyledPageMain>
     <StyledErrorPagesContainer>
       <StyledH1>Error</StyledH1>
       <Page404Illustration />
-      <LinkButton href="/">Home</LinkButton>
+      <LinkButton href={navLinks.home.href}>Home</LinkButton>
     </StyledErrorPagesContainer>
   </StyledPageMain>
 );
 
-export default Custom404;
+export default Custom404Page;
--- pages/500.tsx
+++ pages/500.tsx
@@ -2,13 +2,14 @@ import Page500Illustration from "../Components/Illustrations/Page500Illustration
 import LinkButton from "../Components/LinkButton";
 import StyledH1 from "../Components/StyledComponents/StyledH1";
 import StyledPageMain from "../Components/StyledComponents/StyledPageMain";
+import navLinks from "../data/navLinks";
 
-const Custom404 = () => (
+const Custom500Page = () => (
   <StyledPageMain>
     <StyledH1>Error 500</StyledH1>
     <Page500Illustration />
-    <LinkButton href="/">Home</LinkButton>
+    <LinkButton href={navLinks.home.href}>Home</LinkButton>
   </StyledPageMain>
 );
 
-export default Custom404;
+export default Custom500Page;
